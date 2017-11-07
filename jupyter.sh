dc714a4a451852e1816504c98f3023be51d79eeb
czylabsonasa <czylabson.asa@gmail.com>
2017-11-07 11:37:06 +0100

    progress

--- jupyter.sh
+++ jupyter.sh
@@ -5,35 +5,47 @@
 
 #
 # dd iso image
-#
+# just for remember, always forget it
 dd bs=4M if=/path/to/archlinux.iso of=/dev/sdx status=progress && sync
 
 
-
+# after a fresssh install xubuntu 17.04:
+sudo apt install python3-pip
 #
 # jupyter related
 #
 python3 -m pip install --upgrade pip
 python3 -m pip install --user jupyter
-# ezt .profile-ba
+# mcedit for quick edits:
+# sudo apt install mc 
+# disable the annoying f1 help key for the terminal (setting/special...)
+# ~/.profile, + in a terminal
 export PATH=$PATH:~/.local/bin
-python3 -m pip install --user numpy
+python3 -m pip install --user scipy # also installs numpy
+#python3 -m pip install --user numpy
 python3 -m pip install --user matplotlib
 
 #
 # jupyter/R
 #
+sudo apt install r-base
+#inside R:
 install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ'))
 apt install libssl-dev libcurl3-openssl-dev
 # ezutan ezekkel nem volt problema
 install.packages(c('devtools', 'uuid', 'digest'))
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec()
-
+#
+#test it, now R,python3(numpy) should work with jupyter
+apt install git
+git clone http://github.com/czylabsonasa/barabasinb
 #
 # jupyter/julia
 #
-Pkg.add("IJulia")
+apt install julia
+# inside julia:
+Pkg.add("IJulia") #it will take a while
 Pkg.add("Winston") # a rajzolgatashoz
 
 #
